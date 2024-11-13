88cb2e331ce89d529997a8058376c46a956def5e
sahar387 <sahareftekhari680@gmail.com>
2024-11-13 22:58:40 +0330

    ADD: C# Script

--- Rain-Capture/Assets/PlayerController.cs
+++ Rain-Capture/Assets/PlayerController.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private Rigidbody2D _rb;
+
+    private float _horizontalMovement;
+    private float speed = 5f;
+
+
+
     void Start()
     {
-        
+        _rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        _horizontalMovement = Input.GetAxisRaw("Horizontal");
+
+        if(_horizontalMovement != 0)
+        {
+            _rb.AddForce(new Vector2(_horizontalMovement * speed, 0f));
+        }
     }
 }
